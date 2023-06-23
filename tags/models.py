6fd579e8bbc281bbf342b7c2289ea97929e868bc
kenastark <ikennaudeani@gmail.com>
2023-06-23 09:51:20 +0100

    created new models in tag app

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+# Create your models here.\
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
 
-# Create your models here.
+
+class TaggedItem(models.Model):
+    # What tag applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+# Type (product, video, article)...we can find the table
+# ID....we cna find the record
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField() #ID of that target object
+    content_object = GenericForeignKey() #to get the actual product that this tag is applied to
+    
