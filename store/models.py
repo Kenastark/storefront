1b833f6ebef2c35da3550f8de1e8538ef61e4071
kenastark <ikennaudeani@gmail.com>
2023-06-26 14:16:42 +0100

    created and ran my migrations

--- store/models.py
+++ store/models.py
@@ -11,8 +11,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -65,8 +66,10 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.PositiveSmallIntegerField(null=True)
     #customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True) this creates a 1:1 relationship with Customer
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
 
 
 class Cart(models.Model):
