517b1792e84dc106aa3d54044bf23d9e90dc9808
kenastark <ikennaudeani@gmail.com>
2023-06-27 22:58:58 +0100

    Updated playground/views.py with filtering codes, updated notes

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
-from store.models import Product
+from store.models import *
 
 
 # Create your views here.
 def say_hello(request):
-   exists = Product.objects.filter(pk=0).exists()
+   #keyword=value
+   #queryset = Product.objects.filter(unit_price__range=(20, 30))
 
-   return render(request, 'hello.html', {'name': 'kenastark'}) 
+   #filtering across relationship
+   #queryset = Product.objects.filter(collection__id__range=(1,2,3))
+
+   #filtering with strings
+   #queryset = Product.objects.filter(title__endswith='ed')
+
+   #for dates
+   #queryset = Product.objects.filter(description__isnull=True)
+
+   queryset = Product.objects.filter(inventory__lt=10)
+   queryset1 = Customer.objects.filter(email__icontains='.com')
+   queryset2 = Order.objects.filter(customer_id=1)
+
+   return render(request, 'hello.html', {'name': 'kenastark', 'products': list(queryset), 'customers': list(queryset1), 'orders': list(queryset2)}) 
