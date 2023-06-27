71168636a4e2c05ca2f2c397f098e8080c0d8000
kenastark <ikennaudeani@gmail.com>
2023-06-27 19:55:53 +0100

    added notes, codes for Retrieving Objects

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from store.models import Product
 
 
 # Create your views here.
 def say_hello(request):
-   query_set = Product.objects.all()
+   exists = Product.objects.filter(pk=0).exists()
 
-   for product in query_set:
-      print(product)
    return render(request, 'hello.html', {'name': 'kenastark'}) 
