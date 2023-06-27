3e861b6e05ed25736c9333436800f820c51c3099
kenastark <ikennaudeani@gmail.com>
2023-06-27 19:37:02 +0100

    added code, notes about Managers and QuerySets

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
-
-def calculate():
-   x = 1
-   y = 2
-   return x
 
 # Create your views here.
 def say_hello(request):
-   # return HttpResponse('Hello Kena is king')
-   x = calculate()
+   query_set = Product.objects.all()
+
+   for product in query_set:
+      print(product)
    return render(request, 'hello.html', {'name': 'kenastark'}) 
