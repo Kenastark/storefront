be4591ea3d39ec8c5f145a7dad5c45a555a848c7
kenastark <ikennaudeani@gmail.com>
2023-06-29 18:02:04 +0100

    Aggregating objects lesson, notes

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F #short for Query
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from store.models import *
 
 
@@ -18,8 +17,12 @@ def say_hello(request):
    #for dates
    #queryset = Product.objects.filter(description__isnull=True)
 
-   queryset = Product.objects.prefetch_related('promotions').select_related('collection')[:5]
-   queryset1 = Customer.objects.filter(email__icontains='.com')[:5]
-   queryset2 = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+   #queryset = Product.objects.filter(title__endswith="ed")
+ 
+  # queryset1 = Customer.objects.filter(email__icontains='.com')[:5]
+   #queryset2 = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
 
-   return render(request, 'hello.html', {'name': 'kenastark', 'products': list(queryset), 'customers': list(queryset1), 'orders': list(queryset2)}) 
+
+   result = Product.objects.filter(collection__id=1).aggregate(count=Count('id'), min_price=Min('unit_price'))
+
+   return render(request, 'hello.html', {'name': 'kenastark','result':result}) 
