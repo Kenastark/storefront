41177234c711c97f082d7a8e4ef3d73cc41a821d
kenastark <ikennaudeani@gmail.com>
2023-06-30 09:06:56 +0100

    Calling Database functions code updated

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.db.models import Value, F, Func
-from django.db.models import Concat 
+from django.db.models.functions import Concat
 from store.models import *
 
 
