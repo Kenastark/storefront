cac66c41c7a0d3937a68abd997f63c467d2977ae
kenastark <ikennaudeani@gmail.com>
2023-07-03 22:22:19 +0100

    19 - Querying Generic Relationships

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
-
+from store.models import Product
+from tags.models import TaggedItem
 
 # Create your views here.
 def say_hello(request):
-   
+   content_type = ContentType.objects.get_for_model(Product)
+
+   queryset = TaggedItem.objects \
+   .select_related('tag')\
+   .filter(
+      content_type=content_type,
+      object_id=1
+   )
       
-   return render(request, 'hello.html', {'name': 'kenastark','result': list(queryset)})  
+   return render(request, 'hello.html', {'name': 'kenastark','tags': list(queryset)})  
