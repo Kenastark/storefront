8586001abd8f30387adf11d9e8a8b47ecc4ee434
kenastark <ikennaudeani@gmail.com>
2023-07-05 09:17:24 +0100

    24 -Deleting Objects code added, with code to update id field

--- playground/views.py
+++ playground/views.py
@@ -6,14 +6,21 @@ from tags.models import TaggedItem
 # Create your views here.
 def say_hello(request):
   
-   # collection = Collection.objects.get(pk=14)
-   # collection.featured_product = None
+   # collection = Collection(pk=14)
+   # collection.title = 'Games'
    # collection.save()
-   
-   Collection.objects.filter(pk=15).update(featured_product=None)
 
+   #Collection.objects.filter(pk=15).update(featured_product=None)
+
+   #code to change the id in the collection table
+   #Collection.objects.filter(pk=15).update(id=12)
+
+   #to delete multiple objects
+
+   Collection.objects.filter(id__gt=10).delete()
+   
    #alternative to the four lines above
    #collection = Collection.objects.create(name='a', featured_product_id=1)
    #collection.id
 
-   return render(request, 'hello.html', {'name': 'kenastark',})  
+   return render(request, 'hello.html', {'name': 'kenastark'})  
