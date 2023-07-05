15002d6a9155dad3942b5be6bd3d664c5388277d
kenastark <ikennaudeani@gmail.com>
2023-07-05 08:36:45 +0100

    22 - Creating Objection code and notes added

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product
+from store.models import *
 from tags.models import TaggedItem
 
 # Create your views here.
 def say_hello(request):
   
-   queryset = Product.objects.all()
-   queryset[0]
-   list(queryset)
-      
-   return render(request, 'hello.html', {'name': 'kenastark','tags': list(queryset)})  
+   collection = Collection()
+   collection.title = 'Video Games'
+   collection.featured_product = Product(pk=1) #first method
+   collection.save()
+   
+
+   #alternative to the four lines above
+   #collection = Collection.objects.create(name='a', featured_product_id=1)
+   #collection.id
+
+   return render(request, 'hello.html', {'name': 'kenastark',})  
