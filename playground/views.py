e6b85e6b37ba915c7bd7e2b9a1f2ee30136a7d1d
kenastark <ikennaudeani@gmail.com>
2023-07-05 08:53:52 +0100

    23- Updating Object codes and notes added

--- playground/views.py
+++ playground/views.py
@@ -6,11 +6,11 @@ from tags.models import TaggedItem
 # Create your views here.
 def say_hello(request):
   
-   collection = Collection()
-   collection.title = 'Video Games'
-   collection.featured_product = Product(pk=1) #first method
-   collection.save()
+   # collection = Collection.objects.get(pk=14)
+   # collection.featured_product = None
+   # collection.save()
    
+   Collection.objects.filter(pk=15).update(featured_product=None)
 
    #alternative to the four lines above
    #collection = Collection.objects.create(name='a', featured_product_id=1)
