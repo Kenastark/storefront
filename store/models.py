b8c7317869dc30ce3e42ed8cb3f8b083245c0c55
kenastark <ikennaudeani@gmail.com>
2023-07-11 23:09:21 +0100

    6 - Selecting Related Objects

--- store/models.py
+++ store/models.py
@@ -50,17 +50,16 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
     class Meta:
         db_table = 'store_customers'
         indexes = [
             models.Index(fields=['last_name', 'first_name'])
         ]
-        
-        
-
-    def __str__(self) -> str:
-        return self.title
-
+        ordering = ['first_name', 'last_name']
+   
       
 
 
