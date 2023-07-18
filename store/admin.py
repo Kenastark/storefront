29e1130e853d5952137ca4dc6f49892b368d5d3f
kenastark <ikennaudeani@gmail.com>
2023-07-18 22:51:40 +0100

    9 - Adding Search to the List Page, exercise partly done

--- store/admin.py
+++ store/admin.py
@@ -43,21 +43,23 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
     list_editable = ['membership']
     list_per_page = 10
     ordering = ['first_name', 'last_name'] 
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     @admin.display(ordering='orders_count')
-    def orders(self, order):
+    def orders(self, customer):
+        
         url = (
             reverse('admin:store_order_changelist')
             + '?'
             + urlencode({
-                ''
+                'order_id': str(customer.id)
             })
         )
-        return order.id
+        return format_html('<a href={}>{}</a>', url, customer.id)
 
 #admin.site.register(models.Product)
 
